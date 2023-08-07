fef26731af6efdab980fa63195980bcecd83cccf
nicksolony <54452819+nicksolony@users.noreply.github.com>
2023-08-07 21:40:01 +0000

    Deploying to gh-pages from @ nicksolony/goit-js-hw-10@ab43e63fa62068086c42015f72f166078f60f12c 🚀

--- index.f715dfa2.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".breed-select"),d=document.querySelector(".error");e.classList.add("is-hidden"),d.classList.add("is-hidden")}();
-//# sourceMappingURL=index.f715dfa2.js.map

cc24c721acb83da64f7b8c125686309be5a282ae
nicksolony <nicksolony@gmail.com>
2023-08-07 17:26:51 -0400

    added index.css

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let breedSelectoDropdown = document.querySelector('.breed-select');
+
+function hideSelectMenu() {
+    breedSelectoDropdown.classList.add('is-hidden');
+    
+}
+
+hideSelectMenu();

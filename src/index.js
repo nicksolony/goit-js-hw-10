1c3fbc6c65acc1032b6ef035ef502f80478c641a
nicksolony <nicksolony@gmail.com>
2023-08-08 22:23:51 -0400

    added slim select library

--- src/index.js
+++ src/index.js
@@ -1,37 +1,67 @@
 
 import catApi from "./js/cat-api";
+import SlimSelect from 'slim-select'
 
 
-let breedSelectoDropdown = document.querySelector('.breed-select');
+let breedSelector = document.querySelector('.breed-select');
 let errorMessage = document.querySelector('.error');
 let loaderEl = document.querySelector('.loader');
 let catInfoEl = document.querySelector('.cat-info');
 
 
-breedSelectoDropdown.addEventListener('change', e=>{
-    addBreedImage(e.target.value);
+
+// breedSelector.addEventListener('change', e=>{
+//     addBreedImage(e.target.value);
+// })
+
+let select = new SlimSelect ({
+    select: `#single`,
+    events: {
+        afterChange: (option) => {
+            console.log(option[0])
+            addBreedImage(option[0].value)
+        }
+    },
+    settings: {
+        contentLocation: document.getElementById('local')
+      }
 })
-
-
 
 loadBreeds();
 
 function loadBreeds() {
     showLoader();
     catApi.fetchBreeds()
-    .then(createBreedList)
-    .catch(onError)    
+    .then((createBreedList))
+    .catch(()=>{
+        onError();
+        breedSelector.classList.add('is-hidden');
+        select.destroy();
+    })    
 }
 
+console.log(select);
 
+// function createBreedList(breedsArray) {
+//     breedSelector.classList.remove('is-hidden');
+//     hideLoader();
+//     breedSelector.insertAdjacentHTML("beforeend", (breedsArray.map((breed => {
+//         return `<option value="${breed.id}">${breed.name}</option>`
+//     })).join("")));
+// };
 
 function createBreedList(breedsArray) {
-    breedSelectoDropdown.classList.remove('is-hidden');
+    console.log(breedsArray);
+    // breedSelector.classList.remove('is-hidden');
     hideLoader();
-    breedSelectoDropdown.insertAdjacentHTML("beforeend", (breedsArray.map((breed => {
-        return `<option value="${breed.id}">${breed.name}</option>`
-    })).join("")));
-};
+    select.setData((breedsArray.map((breed)=>{
+        return {
+            text: breed.name,
+            value:breed.id
+        }
+    })
+    ))
+}
 
 
 function addBreedImage(id) {
